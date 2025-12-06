b209ef3ac329e612596024492e72886ec5a72394
victor-yakson <victoryakubu09@gmail.com>
2025-12-06 08:06:50 +0100

    testing deeplink v0.3.8

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,16 @@
-import type { NextConfig } from "next";
+import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  webpack: (config) => {
+    config.resolve = config.resolve || {};
+    config.resolve.alias = {
+      ...(config.resolve.alias || {}),
+      'pino-pretty': false,
+      '@react-native-async-storage/async-storage': false,
+    };
+
+    return config;
+  },
 };
 
 export default nextConfig;
