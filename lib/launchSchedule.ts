9a3c06480fcb59ed4ad05f86a4a8032c4bac23d1
victor-yakson <victoryakubu09@gmail.com>
2026-07-17 16:44:07 +0100

    Update Rico Quant Bot launch time to 11 PM

--- lib/launchSchedule.ts
+++ lib/launchSchedule.ts
@@ -1,4 +1,4 @@
-export const RICO_QUANT_ENGINE_LAUNCH_AT = Date.UTC(2026, 6, 17, 16, 0, 0);
+export const RICO_QUANT_ENGINE_LAUNCH_AT = Date.UTC(2026, 6, 17, 22, 0, 0);
 
 export function isRicoQuantEngineLive(now = Date.now()) {
   return now >= RICO_QUANT_ENGINE_LAUNCH_AT;
